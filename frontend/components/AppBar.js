caaf2d6ec04d0a0481fa478000f07f449f9630f6
sawyerli <sawyerl@oschain.io>
2019-03-22 17:24:26 +0800

    增加友链

--- frontend/components/AppBar.js
+++ frontend/components/AppBar.js
@@ -6,8 +6,11 @@ export default class AppBar extends React.Component {
   render() {
     return <div>
       <div className="mui-appbar">
-        <div className="left">
+        <div className="fl">
           <img  src="https://oschpublicchain.oss-cn-shanghai.aliyuncs.com/dashboard/logo.png" />
+        </div>
+        <div className="fr">
+          <a href="https://www.myoschain.com/">Community</a>
         </div>
       </div>
     </div>
